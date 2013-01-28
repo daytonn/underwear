8178de7af125365cef3620eac34bae5e2702c843
Dayton Nolan <dnolan@gmail.com>
2013-01-27 23:06:39 -0600

    Remove collection and number

--- src/number.js
+++
@@ -1,8 +0,0 @@
-//### times
-if (typeof Number.prototype.times === "undefined") {
-    // Invokes the given iterator function n times.
-    Number.prototype.times = function() {
-        var args = argsWithThis.call(this, arguments);
-        return _.times.apply(this, args);
-    };
-}
